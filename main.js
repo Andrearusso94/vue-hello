e8805ad091458eea6a831385c8374dc4d99249e4
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-26 14:32:01 +0200

    end ex

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /*Descrizione:
 Stampare a schermo un messaggio all’interno di un h1,
  utilizzando i data.*/
+
+ const {createApp} = Vue
+
+createApp({
+    data() {
+        return{
+           title: 'Ciao a tutti',
+        }
+    }
+}).mount('#app')
