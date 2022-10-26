db7d4310c0db587be1ff2c97d7603172936c6a1a
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-26 14:42:36 +0200

    end ex

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 Stampare a schermo un messaggio all’interno di un h1,
  utilizzando i data.*/
 
- const {createApp} = Vue
+const { createApp } = Vue
 
 createApp({
     data() {
-        return{
-           title: 'Ciao a tutti',
-           url: 'https://picsum.photos/200/300'
+        return {
+            title: 'Ciao a tutti',
+            url: 'https://picsum.photos/200/300'
         }
     }
 }).mount('#app')
